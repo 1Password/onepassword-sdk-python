19f33b2d3c5d1d79a14b050bdb560e733a62bf71
Sam Nolan <sam.nolan@agilebits.com>
2024-01-31 09:52:56 -0500

    front end code for initial MR

--- client.py
+++ client.py
@@ -2,45 +2,62 @@ import sys
 import platform
 
 # consts
-SDKSemverVersion = "0010001" # v0.1.0
+SDKVersion = "0010001" # v0.1.0
 DefaultIntegrationName    = "Unknown"
 DefaultIntegrationVersion = "Unknown"
 SDKLanguage           = "Python"
 DefaultRequestLibrary = "net/http"
 
-class Client:
-    def __init__(self, config, Secrets):
-        self.config = config    # ClientConfig
-        self.Secrets = Secrets  # SecretsApi 
+# configuration dictionary
+clientConfigDict = {
+    "SAToken" : "",
+    "Language" : "",
+    "SDKVersion" : "",
+    "IntegrationName" : "",
+    "IntegrationVersion" : "",
+    "RequestLibraryName" : "",
+    "RequestLibraryVersion" : "",
+    "SystemOS" : "",
+    "SystemOSVersion" : "",
+    "SystemArch" : "",
+}
 
-class ClientConfig:
-    def __init__(self, SAToken="", Language="", SDKLanguage="", IntegrationVersion="",
-                 RequestLibraryName="", RequestLibraryVersion="", SystemOS="",
-                 SystemOSVersion="", SystemArch=""):
-        self.SAToken = SAToken
-        self.Language = Language
-        self.SDKVersion = SDKSemverVersion
-        self.IntegrationVersion = IntegrationVersion
-        self.RequestLibraryName = RequestLibraryName
-        self.RequestLibraryVersion = RequestLibraryVersion
-        self.SystemOS = SystemOS
-        self.SystemOSVersion = SystemOSVersion
-        self.SystemArch = SystemArch
+class Client:
+    def __init__(self, auth="", integration_name="", integration_version=""):
+        self.auth = auth   ## onepassword.ServiceAccountCredentials("ops_..."),
+        self.integration_name = integration_name,
+        self.integration_version = integration_version,
 
 def NewDefaultConfig():
     defaultOSVersion = "0.0.0"
-    return ClientConfig(
-		Language = SDKLanguage,
-		SDKVersion= SDKSemverVersion,
-		RequestLibraryName = DefaultRequestLibrary,
-		RequestLibraryVersion = sys.version_info[0] + "." + sys.version_info[1] + "." + sys.version_info[2],
-		SystemOS = platform.system(),
-		SystemArch = platform.architecture()[0],
-		SystemOSVersion = defaultOSVersion
-    )
+
+    newConfigDict = clientConfigDict
+    newConfigDict["Language"] = SDKLanguage
+    newConfigDict["SDKVersion"] = SDKVersion
+    newConfigDict["RequestLibraryName"] = DefaultRequestLibrary
+    newConfigDict["RequestLibraryVersion"] = sys.version_info[0] + "." + sys.version_info[1] + "." + sys.version_info[2]
+    newConfigDict["SystemOS"] = platform.system()
+    newConfigDict["SystemArch"] = platform.architecture()[0],
+    newConfigDict["SystemOSVersion"] = defaultOSVersion
+    
+    return newConfigDict
     
 # def NewClient():
     # TODO
+
+# def createClient():
+    # TODO
+
+def WithServiceAccountToken(token):
+    def assign(client):
+        client.config.SAToken = token
+    return assign
+
+def WithIntegrationInfo(name, version):
+    def assign(client):
+        client.config.IntegrationName = name
+        client.config.IntegrationVersion = version
+    return assign
 
 # def main():
 
