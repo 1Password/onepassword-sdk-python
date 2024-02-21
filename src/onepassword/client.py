e438d0cfe94647cf1382d764f36f5a6c70574bb5
Sam Nolan <sam.nolan@agilebits.com>
2024-02-21 10:55:02 -0500

    buggy python core

--- src/onepassword/client.py
+++ src/onepassword/client.py
@@ -2,6 +2,7 @@ import sys
 import platform
 from core import *
 from secrets_api import Secrets
+import asyncio
 
 SDK_LANGUAGE = "Python"
 SDK_VERSION = "0010001"  # v0.1.0
@@ -19,22 +20,21 @@ class Client:
 
 def new_default_config(auth, integration_name, integration_version):
     client_config_dict = {
-        "SAToken": auth,
-        "Language": SDK_LANGUAGE,
-        "SDKVersion": SDK_VERSION,
-        "IntegrationName": integration_name,
-        "IntegrationVersion": integration_version,
-        "RequestLibraryName": DEFAULT_REQUEST_LIBRARY,
-        "RequestLibraryVersion": str(sys.version_info[0]) + "." + str(sys.version_info[1]) + "." + str(sys.version_info[2]),
-        "SystemOS": platform.system(),
-        "SystemOSVersion": platform.architecture()[0],
-        "SystemArch": DEFAULT_OS_VERSION,
+        "serviceAccountToken": auth,
+        "programmingLanguage": SDK_LANGUAGE,
+        "sdkVersion": SDK_VERSION,
+        "integrationName": integration_name,
+        "integrationVersion": integration_version,
+        "requestLibraryName": DEFAULT_REQUEST_LIBRARY,
+        "requestLibraryVersion": str(sys.version_info[0]) + "." + str(sys.version_info[1]) + "." + str(sys.version_info[2]),
+        "os": platform.system(),
+        "osVersion": platform.architecture()[0],
+        "architecture": DEFAULT_OS_VERSION,
     }
     return client_config_dict
 
-def main():
-    x = InitClient(new_default_config("1","2","3"))
-    print(x)
+async def main():
+    await InitClient(new_default_config("<your-token-here>",DEFAULT_INTEGRATION_NAME,DEFAULT_INTEGRATION_VERSION))
 
 if __name__ == '__main__':
-    main()
+    asyncio.run(main())
--- src/onepassword/core.py
+++ src/onepassword/core.py
@@ -1,44 +1,15 @@
 import ctypes
 import json
 import os
-
-lib = ctypes.CDLL(os.path.join(os.path.dirname(__file__), "../../python-sdk-core/libop_uniffi_core.dylib"))
+import op_uniffi_core
 
 def Invoke(invoke_config):
-
-    invoke = lib.uniffi_op_uniffi_core_fn_func_invoke
-    invoke.restype = ctypes.c_int
-    invoke.argtypes = [ctypes.c_wchar_p]
-
-    return invoke(json.dumps(invoke_config))
+    return op_uniffi_core.invoke(json.dumps(invoke_config))
 
 
 def InitClient(client_config):
-    init_client = lib.uniffi_op_uniffi_core_fn_func_init_client
-    init_client.restype = ctypes.c_int
-    init_client.argtypes = [ctypes.c_wchar_p]
-
-    return init_client(json.dumps(client_config))
+    return op_uniffi_core.init_client(json.dumps(client_config))
 
 
 def ReleaseClient(client_id):
-    release_client = lib.uniffi_op_uniffi_core_fn_func_release_client
-    release_client.restype = ctypes.c_int  # possibly void? return type is Rust Result ()
-    release_client.argtypes = [ctypes.c_wchar_p]
-    
-    return release_client(json.dumps(client_id))
-
-# sample code below for reference
-
-# lib = ctypes.CDLL(os.path.join(os.path.dirname(__file__), "../python-sdk-core/libop_uniffi_core.dylib"))
-# lib.invoke.restype = ctypes.c_char_p  # declare function invoke and declare function's return type
-# lib.invoke.argtypes = [ctypes.c_char_p, ctypes.c_char_p, ctypes.POINTER(ctypes.c_int)]  # declare function parameters
-
-# returned_err = ctypes.c_int(0)
-# id = ctypes.c_long(invoke_config.clientId)
-# arg_arr = json.dumps(invoke_config)
-# result = lib.invoke(id, method.encode("utf-8"), arg_arr, returned_err)
-# if returned_err.value == 1:
-#     raise Exception(result)
-# else:
-#     return result
+    return op_uniffi_core.release_client(json.dumps(client_id))
