c6d1d9ee9e575e6620c3d417f5646ad967106238
Sam Nolan <sam.nolan@agilebits.com>
2024-02-27 23:39:36 -0500

    add example with environmental variable SAToken and comments

---
+++ src/onepassword/example.py
@@ -0,0 +1,17 @@
+import asyncio
+import os
+from onepassword.client import Client, DEFAULT_INTEGRATION_NAME, DEFAULT_INTEGRATION_VERSION
+
+async def main():
+    # Your service account token here
+    token = os.environ("OP_SERVICE_ACCOUNT_TOKEN")
+    
+    # Connect to 1Password
+    client = await Client.authenticate(auth=token, integration_name=DEFAULT_INTEGRATION_NAME, integration_version=DEFAULT_INTEGRATION_VERSION)
+   
+   # Retrieve secret from 1Password
+    value = await client.secrets.resolve("op://Test Login/test_username")
+    print(value)
+
+if __name__ == '__main__':
+    asyncio.run(main())
