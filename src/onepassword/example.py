01e117dfd50c4c38c2f9a6094aaeebe8df6b5a22
Sam Nolan <sam.nolan@agilebits.com>
2024-03-12 13:43:15 -0400

    change verb conjugation in example code comments

--- src/onepassword/example.py
+++ src/onepassword/example.py
@@ -6,10 +6,10 @@ async def main():
     # Replace "OP_SERVICE_ACCOUNT_TOKEN" with the environment variable for your service account token.
     token = os.getenv("OP_SERVICE_ACCOUNT_TOKEN")
     
-    # Connect to 1Password.
+    # Connects to 1Password.
     client = await Client.authenticate(auth=token, integration_name=DEFAULT_INTEGRATION_NAME, integration_version=DEFAULT_INTEGRATION_VERSION)
    
-   # Retrieve a secret from 1Password. Takes a secret reference as input and returns the secret to which it points.
+    # Retrieves a secret from 1Password. Takes a secret reference as input and returns the secret to which it points.
     value = await client.secrets.resolve("op://xw33qlvug6moegr3wkk5zkenoa/bckakdku7bgbnyxvqbkpehifki/foobar")
     print(value)
 
