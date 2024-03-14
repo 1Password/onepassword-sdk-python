e26a9f1d6a395db732ece71a9a8b677d45e63612
Sam Nolan <sam.nolan@agilebits.com>
2024-03-14 10:55:38 -0400

    improve name and version example

--- src/onepassword/example.py
+++ src/onepassword/example.py
@@ -7,7 +7,7 @@ async def main():
     token = os.getenv("OP_SERVICE_ACCOUNT_TOKEN")
     
     # Connects to 1Password.
-    client = await Client.authenticate(auth=token, integration_name="My_Project_Name", integration_version="x.x.x")
+    client = await Client.authenticate(auth=token, integration_name="My 1Password Integration", integration_version="v1.0.0")
    
     # Retrieves a secret from 1Password. Takes a secret reference as input and returns the secret to which it points.
     value = await client.secrets.resolve("op://xw33qlvug6moegr3wkk5zkenoa/bckakdku7bgbnyxvqbkpehifki/foobar")
