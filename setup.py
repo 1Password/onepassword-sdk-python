60d64f01650273c4225490896bffa8239e6ebc81
AndyTitu <andi.titu@agilebits.com>
2024-03-27 10:40:35 +0100

    Improve logic for determining which shared lib to include

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 from setuptools import setup, find_packages
 from sysconfig import get_platform
 import platform
+import os
 
 try:
     from wheel.bdist_wheel import bdist_wheel as _bdist_wheel
@@ -8,16 +9,36 @@ try:
         def finalize_options(self):
             _bdist_wheel.finalize_options(self)
             self.root_is_pure = False
-            self.plat_name = get_platform()
+            # This platform naming is sufficient for distributing this package via source cloning (e.g. pip + GitHub) since the wheel will be built locally 
+            # for each user's platform: https://packaging.python.org/en/latest/specifications/platform-compatibility-tags/#basic-platform-tags
+            self.plat_name = get_platform().translate({'-':'_', '.':'_'})
 except ImportError:
     bdist_wheel = None
 
-def get_shared_library_name():
-    # Return the correct C extension for the given platform
+def get_shared_library_data_to_include():
+    # Return the correct uniffi C shared library extension for the given platform
+    include_path = "lib"
+    if platform.machine() == "x86_64":
+        include_path = os.path.join(include_path, "x86_64")
+    elif platform.machine() == "aarch64" or platform.machine() == "arm64":
+        include_path = os.path.join(include_path, "aarch64")
+
+    c_shared_library_file_name = ""
     if platform.system() == "Darwin":
-        return "libop_uniffi_core.dylib"
+        c_shared_library_file_name = "libop_uniffi_core.dylib"
     elif platform.system() == "Linux":
-        return "libop_uniffi_core.so"
+        c_shared_library_file_name = "libop_uniffi_core.so"
+    elif platform.system() == "Windows":
+        c_shared_library_file_name = "op_uniffi_core.dll"
+
+    c_shared_library_file_name = os.path.join(include_path, c_shared_library_file_name)
+   
+
+    uniffi_bindings_file_name = "op_uniffi_core.py"
+    uniffi_bindings_file_name = os.path.join(include_path, uniffi_bindings_file_name)
+
+    return [c_shared_library_file_name, uniffi_bindings_file_name]
+    
 
 setup(
     name='onepassword',
@@ -30,5 +51,5 @@ setup(
     ),
     package_dir={"": "src"},
     cmdclass={'bdist_wheel': bdist_wheel},
-    package_data={"": [get_shared_library_name()]},
+    package_data={"": get_shared_library_data_to_include()},
 ) 
