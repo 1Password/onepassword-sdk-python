e5040cea94ed102184f4cf438a0d6e402dd40844
Lucy Butcher <lucy.butcher@agilebits.com>
2024-04-10 10:41:20 -0400

    Code comment edits

--- examples/example.py
+++ examples/example.py
@@ -6,10 +6,11 @@ async def main():
     # Gets your service account token from the OP_SERVICE_ACCOUNT_TOKEN environment variable.
     token = os.getenv("OP_SERVICE_ACCOUNT_TOKEN")
     
-    # Connects to 1Password.
+    # Authenticates with your token and connects to 1Password.
     client = await Client.authenticate(auth=token, integration_name="My 1Password Integration", integration_version="v1.0.0")
    
-    # Retrieves a secret from 1Password. Takes a secret reference as input and returns the secret to which it points.
+    # Retrieves a secret from 1Password. 
+	# Takes a secret reference as input and returns the secret to which it points.
     value = await client.secrets.resolve("op://vault/item/field")
     print(value)
 
