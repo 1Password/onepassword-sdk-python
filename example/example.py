5c9024d2c5436f746dde17728e32e82e64ba07b4
Lucy Butcher <89952129+libutcher@users.noreply.github.com>
2024-04-10 11:02:46 -0400

    Fix spacing

--- example/example.py
+++ example/example.py
@@ -10,7 +10,7 @@ async def main():
     client = await Client.authenticate(auth=token, integration_name="My 1Password Integration", integration_version="v1.0.0")
    
     # Retrieves a secret from 1Password. 
-	# Takes a secret reference as input and returns the secret to which it points.
+    # Takes a secret reference as input and returns the secret to which it points.
     value = await client.secrets.resolve("op://vault/item/field")
     print(value)
 
