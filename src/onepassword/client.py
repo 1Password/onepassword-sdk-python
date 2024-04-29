8fbc019636724598a7d8cb0cdf6b8a74ebea3cdd
Horia Culea <horia.culea@agilebits.com>
2024-04-29 12:01:33 +0200

    Refactor code to minimize overhead in autogeneration

--- src/onepassword/client.py
+++ src/onepassword/client.py
@@ -1,18 +1,9 @@
-import platform
 import weakref
 from .core import _init_client, _release_client
 from .secrets_api import Secrets
-
-SDK_LANGUAGE = "Python"
-SDK_VERSION = "0010001"  # v0.1.0
-DEFAULT_INTEGRATION_NAME = "Unknown"
-DEFAULT_INTEGRATION_VERSION = "Unknown"
-DEFAULT_REQUEST_LIBRARY = "net/http"
-DEFAULT_OS_VERSION = "0.0.0"
-
+from .config import new_default_config
 
 class Client:
-
     """authenticate returns an authenticated client or errors if any provided information, including the SA token, is incorrect"""
     """`integration_name` represents the name of your application and `integration_version` represents the version of your application."""
     @classmethod
@@ -24,19 +15,3 @@ class Client:
         self._finalizer = weakref.finalize(self, _release_client, client_id)
 
         return self
-
-# Generates a configuration dictionary with the user's parameters
-def new_default_config(auth, integration_name, integration_version):
-    client_config_dict = {
-        "serviceAccountToken": auth,
-        "programmingLanguage": SDK_LANGUAGE,
-        "sdkVersion": SDK_VERSION,
-        "integrationName": integration_name,
-        "integrationVersion": integration_version,
-        "requestLibraryName": DEFAULT_REQUEST_LIBRARY,
-        "requestLibraryVersion": platform.python_version(),
-        "os": platform.system().lower(),
-        "osVersion": DEFAULT_OS_VERSION,
-        "architecture": platform.machine(),
-    }
-    return client_config_dict
