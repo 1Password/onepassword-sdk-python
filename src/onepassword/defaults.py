40c318302fb2112e80a8dd670cb9139b2cebe011
Horia Culea <horia.culea@agilebits.com>
2024-04-29 13:32:59 +0200

    Fix refactoring artifact

--- src/onepassword/defaults.py
+++ src/onepassword/defaults.py
@@ -1,4 +1,3 @@
-
 import platform
 
 SDK_LANGUAGE = "Python"
--- src/onepassword/test_client.py
+++ src/onepassword/test_client.py
@@ -11,14 +11,14 @@ TOKEN = os.getenv('OP_SERVICE_ACCOUNT_TOKEN')
 # valid
 @pytest.mark.asyncio
 async def test_valid_resolve():
-    client = await onepassword.Client.authenticate(auth=TOKEN, integration_name=onepassword_defaults.onepassword_defaults_INTEGRATION_NAME, integration_version=onepassword_defaults.onepassword_defaults_INTEGRATION_VERSION)
+    client = await onepassword.Client.authenticate(auth=TOKEN, integration_name=onepassword_defaults.DEFAULT_INTEGRATION_NAME, integration_version=onepassword_defaults.DEFAULT_INTEGRATION_VERSION)
     result = await client.secrets.resolve(reference="op://gowwbvgow7kxocrfmfvtwni6vi/6ydrn7ne6mwnqc2prsbqx4i4aq/password")
     assert(result == "test_password_42")
 
 # invalid
 @pytest.mark.asyncio
 async def test_invalid_resolve():
-    client = await onepassword.Client.authenticate(auth=TOKEN, integration_name=onepassword_defaults.onepassword_defaults_INTEGRATION_NAME, integration_version=onepassword_defaults.onepassword_defaults_INTEGRATION_VERSION)
+    client = await onepassword.Client.authenticate(auth=TOKEN, integration_name=onepassword_defaults.DEFAULT_INTEGRATION_NAME, integration_version=onepassword_defaults.DEFAULT_INTEGRATION_VERSION)
     with pytest.raises(Exception, match="error resolving secret reference: secret reference is not prefixed with \"op://\""):
         await client.secrets.resolve(reference="invalid_reference")
 
@@ -27,42 +27,42 @@ async def test_invalid_resolve():
 # valid
 @pytest.mark.asyncio
 async def test_good_client_construction():
-    client = await onepassword.Client.authenticate(auth=TOKEN, integration_name=onepassword_defaults.onepassword_defaults_INTEGRATION_NAME, integration_version=onepassword_defaults.onepassword_defaults_INTEGRATION_VERSION)
+    client = await onepassword.Client.authenticate(auth=TOKEN, integration_name=onepassword_defaults.DEFAULT_INTEGRATION_NAME, integration_version=onepassword_defaults.DEFAULT_INTEGRATION_VERSION)
     assert(client.config['serviceAccountToken'] == TOKEN)
-    assert(client.config['integrationName'] == onepassword_defaults.onepassword_defaults_INTEGRATION_NAME)
-    assert(client.config['integrationVersion'] == onepassword_defaults.onepassword_defaults_INTEGRATION_VERSION)
+    assert(client.config['integrationName'] == onepassword_defaults.DEFAULT_INTEGRATION_NAME)
+    assert(client.config['integrationVersion'] == onepassword_defaults.DEFAULT_INTEGRATION_VERSION)
 
 # invalid
 @pytest.mark.asyncio
 async def test_client_construction_no_auth():
     with pytest.raises(Exception, match='invalid client configuration: encountered the following errors: service account token was not specified; service account token had invalid format'):
-        await onepassword.Client.authenticate(auth="", integration_name=onepassword_defaults.onepassword_defaults_INTEGRATION_NAME, integration_version=onepassword_defaults.onepassword_defaults_INTEGRATION_VERSION)
+        await onepassword.Client.authenticate(auth="", integration_name=onepassword_defaults.DEFAULT_INTEGRATION_NAME, integration_version=onepassword_defaults.DEFAULT_INTEGRATION_VERSION)
 
 # invalid   
 @pytest.mark.asyncio
 async def test_client_construction_no_name():
     with pytest.raises(Exception, match='invalid client configuration: encountered the following errors: integration name was not specified'):
-        await onepassword.Client.authenticate(auth=TOKEN, integration_name="", integration_version=onepassword_defaults.onepassword_defaults_INTEGRATION_VERSION)
+        await onepassword.Client.authenticate(auth=TOKEN, integration_name="", integration_version=onepassword_defaults.DEFAULT_INTEGRATION_VERSION)
 
 # invalid    
 @pytest.mark.asyncio
 async def test_client_construction_no_version():
     with pytest.raises(Exception, match='invalid client configuration: encountered the following errors: integration version was not specified'):
-        await onepassword.Client.authenticate(auth=TOKEN, integration_name=onepassword_defaults.onepassword_defaults_INTEGRATION_NAME, integration_version="")
+        await onepassword.Client.authenticate(auth=TOKEN, integration_name=onepassword_defaults.DEFAULT_INTEGRATION_NAME, integration_version="")
 
 ## test config function
     
 # valid
-def test_good_new_onepassword_defaults_config():
-    config = onepassword.new_onepassword_defaults_config(auth=TOKEN, integration_name=onepassword_defaults.onepassword_defaults_INTEGRATION_NAME, integration_version=onepassword_defaults.onepassword_defaults_INTEGRATION_VERSION)
+def test_good_new_onepassword_default_config():
+    config = onepassword.new_default_config(auth=TOKEN, integration_name=onepassword_defaults.DEFAULT_INTEGRATION_NAME, integration_version=onepassword_defaults.DEFAULT_INTEGRATION_VERSION)
     
     assert(config["serviceAccountToken"] == TOKEN)
     assert(config["programmingLanguage"] == onepassword_defaults.SDK_LANGUAGE)
     assert(config["sdkVersion"] == onepassword_defaults.SDK_VERSION)
-    assert(config["integrationName"] == onepassword_defaults.onepassword_defaults_INTEGRATION_NAME)
-    assert(config["integrationVersion"] == onepassword_defaults.onepassword_defaults_INTEGRATION_VERSION)
-    assert(config["requestLibraryName"] == onepassword_defaults.onepassword_defaults_REQUEST_LIBRARY)
+    assert(config["integrationName"] == onepassword_defaults.DEFAULT_INTEGRATION_NAME)
+    assert(config["integrationVersion"] == onepassword_defaults.DEFAULT_INTEGRATION_VERSION)
+    assert(config["requestLibraryName"] == onepassword_defaults.DEFAULT_REQUEST_LIBRARY)
     assert(config["requestLibraryVersion"] == platform.python_version())
     assert(config["os"] == platform.system().lower())
-    assert(config["osVersion"] == onepassword_defaults.onepassword_defaults_OS_VERSION)
+    assert(config["osVersion"] == onepassword_defaults.DEFAULT_OS_VERSION)
     assert(config["architecture"] == platform.machine())
