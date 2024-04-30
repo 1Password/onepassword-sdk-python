44d70d005666e763d94c6f8a203e11de2376f407
Omar Miraj <omar.miraj@agilebits.com>
2024-04-30 15:48:58 -0400

    Revert "Initial commit"
    This reverts commit bd5a6c56ce53321e243fc969e5dd4de49e61fe20.

--- src/onepassword/client.py
+++ src/onepassword/client.py
@@ -1,21 +1,17 @@
 import weakref
 from .core import _init_client, _release_client
-from .defaults import new_default_config
 from .secrets import Secrets
-from .items import Items
-
+from .defaults import new_default_config
 
 class Client:
+    """authenticate returns an authenticated client or errors if any provided information, including the SA token, is incorrect"""
+    """`integration_name` represents the name of your application and `integration_version` represents the version of your application."""
+    @classmethod
+    async def authenticate(cls, auth, integration_name, integration_version):
+        self = cls()
+        self.config = new_default_config(auth=auth, integration_name=integration_name, integration_version=integration_version)
+        client_id = int(await _init_client(self.config))
+        self.secrets = Secrets(client_id)
+        self._finalizer = weakref.finalize(self, _release_client, client_id)
 
-	@classmethod
-	async def authenticate(cls, auth, integration_name, integration_version):
-		self = cls()
-		
-		self.config = new_default_config(auth=auth, integration_name=integration_name, integration_version=integration_version)
-		client_id = int(await _init_client(self.config))
-
-		self.secrets = Secrets(client_id)
-		self.items = Items(client_id)
-		self._finalizer = weakref.finalize(self, _release_client, client_id)
-
-		return self
+        return self
--- src/onepassword/secrets.py
+++ src/onepassword/secrets.py
@@ -1,21 +1,17 @@
 from .core import _invoke
 
+"""Secrets represents all operations the SDK client can perform on 1Password secrets."""
 class Secrets:
-  def __init__(self, client_id):
-    self.client_id = client_id
+    def __init__(self, client_id):
+        self.client_id = client_id
 
-  async def resolve(self, secret_reference): 
-
-   response = await _invoke({
-		"clientId": self.client_id,
-	   "invocation": {
-			"name": "Resolve",
-			"parameters": {  
-			   "secret_reference": secret_reference, 
-			}
-			}
-		})
-
-   return response
-
-
+    """resolve returns the secret the provided reference points to."""
+    async def resolve(self, reference):
+        response = await _invoke({
+            "clientId": self.client_id,
+            "invocation": {
+                "name": "Resolve",
+                "parameters": reference,
+                }
+            })
+        return response
