7a2247bd4e5f4248b03979dafb6b682eb4fba93b
AndyTitu <andi.titu@agilebits.com>
2024-05-14 16:18:19 +0300

    Bump version in setup.py

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ def get_shared_library_data_to_include():
 
 setup(
     name="onepassword",
-    version="0.1.0-beta.1",
+    version="0.1.0-beta.6",
     author="1Password",
     description="The 1Password Python SDK offers programmatic read access to your secrets in 1Password in an interface native to Python.",
     url="https://github.com/1Password/onepassword-sdk-python",
