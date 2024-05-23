121cadb2338172f3dfeaad1fed92740e1eda1ccb
1PasswordSDKBot <it-github-sdk-bot@agilebits.com>
2024-05-23 15:56:07 +0000

    Update core to version a892358e

--- src/onepassword/items.py
+++ src/onepassword/items.py
@@ -1,16 +1,13 @@
 from .core import _invoke
-import json
+from json import loads
 from .types import Item
 
 
 class Items:
-    """Contains all operations the SDK client can perform on 1Password items."""
-
     def __init__(self, client_id):
         self.client_id = client_id
 
     async def create(self, item):
-        """Create a new item"""
         response = await _invoke(
             {
                 "clientId": self.client_id,
@@ -22,28 +19,24 @@ class Items:
                 },
             }
         )
-        result = Item(**json.loads(response))
-        return result
+        return Item(**loads(response))
 
     async def get(self, vault_id, item_id):
-        """Get an item by vault and item ID"""
         response = await _invoke(
             {
                 "clientId": self.client_id,
                 "invocation": {
                     "name": "Get",
                     "parameters": {
-                        "vault_id": vault_id,
                         "item_id": item_id,
+                        "vault_id": vault_id,
                     },
                 },
             }
         )
-        result = Item(**json.loads(response))
-        return result
+        return Item(**loads(response))
 
     async def update(self, item):
-        """Update an existing item. Warning: Only text and concealed fields are currently supported. Other fields will be permanently lost when you update an item."""
         response = await _invoke(
             {
                 "clientId": self.client_id,
@@ -55,19 +48,17 @@ class Items:
                 },
             }
         )
-        result = Item(**json.loads(response))
-        return result
+        return Item(**loads(response))
 
     async def delete(self, vault_id, item_id):
-        """Delete an item. Warning:  Information saved in fields other than text and concealed fields will be permanently lost."""
         await _invoke(
             {
                 "clientId": self.client_id,
                 "invocation": {
                     "name": "Delete",
                     "parameters": {
-                        "vault_id": vault_id,
                         "item_id": item_id,
+                        "vault_id": vault_id,
                     },
                 },
             }
--- src/onepassword/secrets.py
+++ src/onepassword/secrets.py
@@ -3,13 +3,10 @@ from json import loads
 
 
 class Secrets:
-    """Contains all operations the SDK client can perform on 1Password secrets."""
-
     def __init__(self, client_id):
         self.client_id = client_id
 
     async def resolve(self, secret_reference):
-        """Resolve the secret reference to a secret."""
         response = await _invoke(
             {
                 "clientId": self.client_id,
@@ -21,4 +18,4 @@ class Secrets:
                 },
             }
         )
-        return loads(response)
+        return str(loads(response))
