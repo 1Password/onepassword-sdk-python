c7807ce4b9c0d61365285cba8d6df60680d2aac6
AndyTitu <andi.titu@agilebits.com>
2024-06-05 10:13:50 +0200

    Have ruff ignore the  unable to detect undefined names error as we are aware and fine with the risks

--- src/onepassword/__init__.py
+++ src/onepassword/__init__.py
@@ -3,7 +3,7 @@ from .client import Client
 from .defaults import DEFAULT_INTEGRATION_NAME, DEFAULT_INTEGRATION_VERSION
 from .secrets import Secrets
 from .items import Items
-from .types import *
+from .types import * # noqa F403
 
 
 __all__ = [
