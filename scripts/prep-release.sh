a6b3ec03e4692630c6478f2f52a6fb3a63381000
Omar Miraj <omar.miraj@agilebits.com>
2024-06-12 08:56:32 -0400

    Fix quotes

--- scripts/prep-release.sh
+++ scripts/prep-release.sh
@@ -85,5 +85,5 @@ echo "${changelog_content}" >> "${changelog_file}"
 echo "Release has been prepared..
 Make sure to double check version/build numbers in their appropriate files and
 changelog is correctly filled out.
-Once confirmed, run `make release` to release the SDK!"
+Once confirmed, run 'make release' to release the SDK!"
 
