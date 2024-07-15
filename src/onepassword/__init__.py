e1ce16809edae086bcdb7c031ecca2369b4947aa
1PasswordSDKBot <it-github-sdk-bot@agilebits.com>
2024-07-15 15:21:59 +0000

    Update core to version 02c78574

--- src/onepassword/__init__.py
+++ src/onepassword/__init__.py
@@ -1,11 +1,12 @@
 # AUTO-GENERATED
 from .client import Client
 from .defaults import DEFAULT_INTEGRATION_NAME, DEFAULT_INTEGRATION_VERSION
-from .types import * # noqa F403
+from .types import *  # noqa F403
 from .secrets import Secrets
 from .items import Items
 
-import sys 
+
+import sys
 import inspect
 import typing
 
@@ -19,5 +20,8 @@ __all__ = [
 
 for name, obj in inspect.getmembers(sys.modules["onepassword.types"]):
     # Add all classes and instances of typing.Literal defined in types.py.
-    if (inspect.isclass(obj) and inspect.getmodule(obj) == sys.modules["onepassword.types"]) or type(eval(name)) == typing._LiteralGenericAlias:
+    if (
+        inspect.isclass(obj)
+        and inspect.getmodule(obj) == sys.modules["onepassword.types"]
+    ) or type(eval(name)) == typing._LiteralGenericAlias:
         __all__.append(name)
