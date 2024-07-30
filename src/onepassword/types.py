89d4d8f2cfb3d87f0f7bbf01596f098046bc4a9a
1PasswordSDKBot <it-github-sdk-bot@agilebits.com>
2024-07-30 18:32:23 +0000

    Update core to version cc8ee14e

--- src/onepassword/types.py
+++ src/onepassword/types.py
@@ -9,12 +9,42 @@ from typing import List, Literal, Optional
 
 
 class Item(BaseModel):
+    """
+    Represents a 1Password item.
+    """
+
     id: str
+    """
+    The item's ID
+    """
     title: str
+    """
+    The item's title
+    """
     category: ItemCategory
+    """
+    The item's category
+    """
     vault_id: str
+    """
+    The ID of the vault where the item is saved
+    """
+    tags: List[str]
+    """
+    The item's tags
+    """
     fields: List[ItemField]
+    """
+    The item's fields
+    """
     sections: List[ItemSection]
+    """
+    The item's sections
+    """
+    version: int
+    """
+    The item's version
+    """
 
 
 ItemCategory = Literal[
@@ -45,17 +75,104 @@ ItemCategory = Literal[
 ]
 
 
-class ItemField(BaseModel):
-    id: str
+class ItemCreateParams(BaseModel):
+    category: ItemCategory
+    """
+    The item's category
+    """
+    vault_id: str
+    """
+    The ID of the vault where the item is saved
+    """
     title: str
+    """
+    The item's title
+    """
+    fields: List[ItemField]
+    """
+    The item's fields
+    """
+    sections: List[ItemSection]
+    """
+    The item's sections
+    """
+    tags: List[str]
+    """
+    The item's tags
+    """
+
+
+class ItemField(BaseModel):
+    """
+    Represents a field within an item.
+    """
+
+    id: str
+    """
+    The field's ID
+    """
+    title: str
+    """
+    The field's title
+    """
     section_id: Optional[str]
+    """
+    The ID of the section containing the field. Built-in fields such as usernames and passwords don't require a section.
+    """
     field_type: ItemFieldType
+    """
+    The field's type
+    """
     value: str
+    """
+    The string representation of the field's value
+    """
+    details: Optional[ItemFieldDetails]
+    """
+    Field type-specific attributes.
+    """
 
 
-ItemFieldType = Literal["Text", "Concealed", "Unsupported"]
+class ItemFieldDetailsOtp(BaseModel):
+    type: Literal["Otp"]
+    content: OtpFieldDetails
+
+
+ItemFieldDetails = ItemFieldDetailsOtp
+"""
+Field type-specific attributes.
+"""
+
+ItemFieldType = Literal[
+    "Text", "Concealed", "CreditCardType", "Phone", "Url", "Totp", "Unsupported"
+]
 
 
 class ItemSection(BaseModel):
+    """
+    A section groups together multiple fields in an item.
+    """
+
     id: str
+    """
+    The section's unique ID
+    """
     title: str
+    """
+    The section's title
+    """
+
+
+class OtpFieldDetails(BaseModel):
+    """
+    Additional attributes for OTP fields.
+    """
+
+    code: Optional[str]
+    """
+    The OTP code, if successfully computed
+    """
+    error_message: Optional[str]
+    """
+    The error message, if the OTP code could not be computed
+    """
