0f52cca588f252c3307e21c0aca514dfb380a0b3
Omar Miraj <omar.miraj@agilebits.com>
2024-08-15 10:09:21 -0400

    Revert "Remove optional from auth and add it directly to the new_defualt_config"
    This reverts commit f89d85de939431aa9f3880c65f490dbb62dc622d.

--- src/onepassword/client.py
+++ src/onepassword/client.py
@@ -15,7 +15,7 @@ class Client:
     @classmethod
     async def authenticate(cls, auth, integration_name, integration_version):
         config = new_default_config(
-            auth=auth or "", # Convert None to empty string from os.getenv  
+            auth=auth,
             integration_name=integration_name,
             integration_version=integration_version,
         )
