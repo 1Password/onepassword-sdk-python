cfa3b8ab4d2c57b2db8feef920956fd44ba44303
Omar Miraj <omar.miraj@agilebits.com>
2024-09-26 07:38:33 -0400

    move building of wheels to prep release script

--- src/release/scripts/prep-release.sh
+++ src/release/scripts/prep-release.sh
@@ -5,6 +5,16 @@
 output_version_file="src/release/version.py"
 version_template_file="src/release/templates/version.tpl.py"
 
+# The list of python verisons the SDKs release for
+python_versions=("$@")
+
+# Minimum glibc version we support
+glibc_version=2-32
+
+# These versions are being supported due to the SDKs supporting Python 3.9+
+macOS_version_x86_64=10.9
+macOS_version_arm64=11.0
+
 # Extracts the current build/version number for comparison and backup 
 current_build=$(awk -F "['\"]" '/SDK_BUILD_NUMBER =/{print $2}' "$output_version_file")
 current_version=$(awk -F "['\"]" '/SDK_VERSION =/{print $2}' "$output_version_file")
@@ -73,6 +83,41 @@ update_and_validate_build() {
     done
 }
 
+
+build_wheels() {
+    os_platform=$1
+    machine_platform=$2
+
+    export PYTHON_OS_PLATFORM=$os_platform
+    export PYTHON_MACHINE_PLATFORM=$machine_platform
+
+    case "$os_platform" in 
+        Darwin)
+            version=
+            if [[ "$machine_platform" == "x86_64" ]]; then
+                version=$macOS_version_x86_64
+            else
+                version=$macOS_version_arm64
+            fi
+
+            export _PYTHON_HOST_PLATFORM="macosx-${version}-${PYTHON_MACHINE_PLATFORM}"
+            ;;
+        Linux)
+            export _PYTHON_HOST_PLATFORM="manylinux-${glibc_version}-${PYTHON_MACHINE_PLATFORM}"
+            ;;
+        Windows)
+            export _PYTHON_HOST_PLATFORM="win-${PYTHON_MACHINE_PLATFORM}"
+            ;;
+        *)
+            echo "Unsupported OS: $os_platform"
+            exit 1
+            ;;
+    esac
+
+    pyenv exec python setup.py bdist_wheel
+    rm -rf build
+}
+
 # Ensure working directory is clean
 enforce_latest_code
 
@@ -81,6 +126,19 @@ update_and_validate_version
 
 # Update and validate the build number
 update_and_validate_build 
+
+# Acquire the wheels for different OS
+for version in "${python_versions[@]}"; do
+pyenv local $version
+build_wheels Darwin x86_64
+build_wheels Darwin arm64
+build_wheels Linux x86_64
+build_wheels Linux aarch64
+build_wheels Windows amd64
+done
+
+# Build Source as well incase wheels fails, pypi can install this as backup (standard practice)
+python3 -m build --sdist
 
 # Update version & build number in version.py
 sed -e "s/{{ build }}/$build/" -e "s/{{ version }}/$version/" "$version_template_file" > "$output_version_file"
