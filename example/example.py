6accb0a82a44dbd497139436cf2aa5d5fbef3249
Horia Culea <horia.culea@agilebits.com>
2025-06-19 10:48:50 +0200

    Also reformat the get item invocation

--- example/example.py
+++ example/example.py
@@ -17,14 +17,14 @@ async def main():
 
     # Connects to 1Password.
     client = await Client.authenticate(
-        auth="",
+        auth="AgHouUoF1O06H1z9hJjQofEveT3ITHGYmTEvCucRNtokepcEC7vLiq5L95Dc42Fwljzj5tsqWRujsUHOkyuLPOnn_BrJu0H81x2CBKnerwMdlgc",
         # Set the following to your own integration name and version.
         integration_name="My 1Password Integration",
         integration_version="v1.0.0",
     )
     # [developer-docs.sdk.python.client-initialization]-end
 
-    secret = await client.secrets.resolve("op://test/AcmeCorp/credential")
+    secret = await client.items.get("54jd5fopcp34u2rqqypee6uofm", "it76yngyalveqbmbbz4ijhwnrq")
     print(f"Secret resolved: {secret}")
 
 if __name__ == "__main__":
