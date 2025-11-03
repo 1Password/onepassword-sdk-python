f8441dc09ad945c520dd112c61cf8fb78e8b9c98
Eduard Filip <eddy.filip@agilebits.com>
2025-11-03 15:53:04 +0100

    Add support for WIndows (#190)
    This enables the SDK to authenticate via the 1Password app on Windows.

--- src/onepassword/desktop_core.py
+++ src/onepassword/desktop_core.py
@@ -23,6 +23,13 @@ def find_1password_lib_path():
 			"/opt/1Password/libop_sdk_ipc_client.so",
 			"/snap/bin/1password/libop_sdk_ipc_client.so",
         ]
+    elif os_name == "Windows":
+        locations = [
+            str(Path.home() / r"AppData\Local\1Password\op_sdk_ipc_client.dll"),
+            r"C:\Program Files\1Password\app\8\op_sdk_ipc_client.dll",
+			r"C:\Program Files (x86)\1Password\app\8\op_sdk_ipc_client.dll",
+            str(Path.home() / r"AppData\Local\1Password\app\8\op_sdk_ipc_client.dll"),
+        ]
     else:
         raise OSError(f"Unsupported operating system: {os_name}")
 
