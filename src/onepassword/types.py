917ac77054bdbb791ea3162e8b8ab5e91941ce5e
Andi Titu <45081667+AndyTitu@users.noreply.github.com>
2026-02-06 16:18:10 +0100

    Revert name to title in comment

--- src/onepassword/types.py
+++ src/onepassword/types.py
@@ -1365,7 +1365,7 @@ class VaultOverview(BaseModel):
     """
     attribute_version: int = Field(alias="attributeVersion")
     """
-    The attribute version number of the vault. It gets incremented whenever vault presentation information changes, such as its name or icon.
+    The attribute version number of the vault. It gets incremented whenever vault presentation information changes, such as its title or icon.
     """
     created_at: Annotated[
         datetime,
