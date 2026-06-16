181059cd342c2379a1b6d50b22daa77eceaad557
1PasswordSDKBot <it-github-sdk-bot@agilebits.com>
2026-06-16 15:34:25 +0000

    Update core to version 1710cc48

--- src/onepassword/types.py
+++ src/onepassword/types.py
@@ -234,6 +234,7 @@ class GroupState(str, Enum):
 class VaultAccessorType(str, Enum):
     USER = "user"
     GROUP = "group"
+    BROKER = "broker"
 
 
 class VaultAccess(BaseModel):
@@ -1329,7 +1330,7 @@ class Vault(BaseModel):
     """
     attribute_version: int = Field(alias="attributeVersion")
     """
-    The attribute version number of the vault. It gets incremented whenever vault presentation information changes, such as its title or icon.
+    The attribute version number of the vault. It gets incremented whenever vault presentation information changes, such as its name or icon.
     """
     access: Optional[List[VaultAccess]] = Field(default=None)
     """
@@ -1395,7 +1396,7 @@ class VaultOverview(BaseModel):
     """
     attribute_version: int = Field(alias="attributeVersion")
     """
-    The attribute version number of the vault. It gets incremented whenever vault presentation information changes, such as its title or icon.
+    The attribute version number of the vault. It gets incremented whenever vault presentation information changes, such as its name or icon.
     """
     created_at: Annotated[
         datetime,
